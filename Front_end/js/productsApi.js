5f666f2ca5fb8c26a78f2da54fb5090c3088454d
Gaelle Loyse <gaelleloyse@yahoo.fr>
2021-08-26 21:00:43 +0200

    26

--- Front_end/js/productsApi.js
+++ Front_end/js/productsApi.js
@@ -1,8 +1,23 @@
-let camera;
+const cameraCard = document.getElementById("cardCamera");
 
-const fetchCamera = async () => {
-  camera = await fetch("http://localhost:3000/api/cameras")
-    .then((resp) => resp.json())
-    .then((data) => console.log(data));
-};
-fetchCamera();
+var cameras;
+
+//Appel produits // Requete APIasync
+async function fetchCameras() {
+  let rep = await fetch("http://localhost:3000/api/cameras", { method: "GET" });
+  let response = await rep.json();
+  return response;
+}
+cameras = fetchCameras();
+console.log("blabla ", cameras);
+
+function productCard() {
+  for (camera in cameras) {
+    cardCamera.innerHTML = `
+          <img class="" src="${camera.imageUrl}"/>
+          <h5 class="" >${camera.name}</h5>
+          <p class="" >${camera.description}</p>
+          <p class="">${camera.price}</p>
+          `;
+  }
+}
