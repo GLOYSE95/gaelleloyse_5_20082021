2eb1b9c9f531f9bd2619b2e8667ac81946931ab8
Gaelle Loyse <gaelleloyse@yahoo.fr>
2021-09-02 22:37:28 +0200

    dfgvb

--- Front_end/js/product.js
+++ Front_end/js/product.js
@@ -1,53 +1,63 @@
-const cameraCard = document.getElementById("selectCamera");
-let cameras;
-let camera;
-let theProduct;
-let i;
-let textTheProduct = `
-  <div class="oneCard">
-    <div class="contain-imgCamera">
-      <img class="imgCamera" src=" " alt="photo" />
-    </div>
-    <div class="textCamera">
-      <h3 class=" "> </h3>
-      <p class=" "> </p>
-      <p class="priceCamera"> Prix : €</p>
-    </div>
-  </div>
-  `;
+// let cameras;
+// let camera;
+// let theProduct;
+// let i;
+// let textTheProduct = `
+//   <div class="oneCard">
+//     <div class="contain-imgCamera">
+//       <img class="imgCamera" src=" " alt="photo" />
+//     </div>
+//     <div class="textCamera">
+//       <h3 class=" "> </h3>
+//       <p class=" "> </p>
+//       <p class="priceCamera"> Prix : €</p>
+//     </div>
+//   </div>
+//   `;
 
-// Requete API récupération des données
-const fetchCam = async () => {
-  await fetch("http://localhost:3000/api/cameras")
-    .then((res) => res.json())
-    .then((data) => (cameras = data));
-  //return cameras;
+// // Requete API récupération des données
+// const fetchCam = async () => {
+//   await fetch("http://localhost:3000/api/cameras")
+//     .then((res) => res.json())
+//     .then((data) => (cameras = data));
+//   //return cameras;
+// };
+// fetchCam();
+
+// //recupération Id produit selectionné par url
+// var queryUrlId = window.location.search;
+// //console.log(queryUrlId);
+
+// //recupération valeur Id produit selectionné
+// var idObject = queryUrlId.slice(4);
+// //console.log(idObject);
+
+// function selectProduct(x) {
+//   fetch(`http://localhost:3000/api/cameras/` + idObject)
+//     .then((res) => res.json())
+//     .then((data) => (laCamera = data));
+// }
+// selectProduct();
+
+// // //recupération des données du produit selectionné
+// // const selectProduct = async () => {
+// //   await fetchCam();
+
+// //   //console.log(cameras);
+// //   //console.log(cameras[3]._id);
+
+// //   for (let i in cameras) {
+// //     if (cameras[i]._id == idObject) {
+// //       theProduct = cameras[i];
+// //       console.table(theProduct);
+// //     } else {
+// //       console.log("produit non correspondant");
+// //     }
+// //   }
+// // };
+// // selectProduct();
+
+const cameraId = () => {
+  return new URL(location.href).searchParams.get("id");
 };
-fetchCam();
-
-//recupération Id produit selectionné par url
-var queryUrlId = window.location.search;
-//console.log(queryUrlId);
-
-//recupération valeur Id produit selectionné
-var idObject = queryUrlId.slice(4);
-//console.log(idObject);
-
-//recupération des données du produit selectionné
-const selectProduct = async () => {
-  await fetchCam();
-
-  //console.log(`${cameras}`);?????
-  //console.log(cameras);
-  //console.log(cameras[3]._id);
-
-  for (let i in cameras) {
-    if (cameras[i]._id == idObject) {
-      theProduct = cameras[i];
-      console.table(theProduct);
-    } else {
-      console.log("produit non correspondant");
-    }
-  }
-};
-selectProduct();
+console.log(cameraId());
--- Front_end/js/productsApi.js
+++ Front_end/js/productsApi.js
@@ -1,4 +1,6 @@
 // pointeur index.html
+const CardProductOne = document.getElementById("selectCamera");
+
 const cameraCard = document.getElementById("cardCamera");
 const cardSelectCamera = document.getElementById("cardSelectCamera");
 const linkProduct = document.getElementsByClassName("oneCard");
@@ -15,6 +17,13 @@ const centEuro = (x) => {
   return (x = x / 100);
 };
 
+let idCamera;
+function showProduct(id) {
+  idCamera = id;
+  window.open("./produits.html");
+  console.log(idCamera);
+}
+
 //Appel produits innerHtml
 const showProducts = async () => {
   await fetchCam();
@@ -22,15 +31,17 @@ const showProducts = async () => {
   cameraCard.innerHTML = cameras
     .map(
       (camera) =>
+        //href="./produits.html?id=${camera._id}
         `
-      <a href="./produits.html?id=${camera._id}">
-        <div class="oneCard">
+      <a onClick="showProduct('${camera._id}')">
+        <div class="oneCard")">
           <div class="contain-imgCamera">
               <img class="imgCamera" src="${camera.imageUrl}" alt="photo" />
           </div>
           <div class="textCamera">
             <h3 class=" ">${camera.name}</h3>
           <p class=" " >${camera.description}</p>
+          <button>BLABLA</button>
             <p class="priceCamera"> Prix : ${centEuro(camera.price)} €</p>
            </div>
        </div>
@@ -41,3 +52,38 @@ const showProducts = async () => {
 };
 
 showProducts();
+
+const fetchCamId = async () => {
+  if (idCamera != null) {
+    await fetch(`http://localhost:3000/api/cameras/` + idCamera)
+      .then((res) => res.json())
+      .then((data) => (cameras = data));
+  }
+};
+
+const montreMoiLeProduit = async () => {
+  await fetchCamId();
+
+  CardProductOne.innerHTML = cameras
+    .map(
+      (camera) =>
+        //href="./produits.html?id=${camera._id}
+        `
+      <a href="./produits.html?id=${camera._id}">
+        <div class="oneCard")">
+          <div class="contain-imgCamera">
+              <img class="imgCamera" src="${camera.imageUrl}" alt="photo" />
+          </div>
+          <div class="textCamera">
+            <h3 class=" ">${camera.name}</h3>
+          <p class=" " >${camera.description}</p>
+          <button>BLABLA</button>
+            <p class="priceCamera"> Prix : ${centEuro(camera.price)} €</p>
+           </div>
+       </div>
+     </a>
+   `
+    )
+    .join("");
+};
+montreMoiLeProduit();
