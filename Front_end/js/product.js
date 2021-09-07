523ac81543bed4e67f5c1457151445404e519e17
Gaelle Loyse <gaelleloyse@yahoo.fr>
2021-09-07 11:46:13 +0200

    données produits

--- Front_end/js/product.js
+++ Front_end/js/product.js
@@ -12,7 +12,6 @@ const fetchCamera = async () => {
   await fetch(`http://localhost:3000/api/cameras/${getCameraId()}`)
     .then((res) => res.json())
     .then((data) => (cam = data));
-  console.log(cam);
 };
 
 //arrondir le prix
@@ -24,13 +23,54 @@ const centEuro = (x) => {
 const displayCamera = async () => {
   await fetchCamera();
 
+  //let pointeurOptions = document.getElementById("optionsProduct");
+  options = cam.lenses;
+
+  // nbrOptions = options.length;
+  // if (nbrOptions < 3) {
+  //   document.getElementById("opt1").textContent = "Pas d'option pour ce produit";}
+  // for (let i = 0; i < options.lenght; i++) {
+  //   let create = document.createElement("option");
+  //   pointeurOptions.appendChild(create);
+  //   create.textContent = cam.lenses[i];
+  // }
+
   document.getElementById("imgProduct").src = cam.imageUrl;
   document.getElementById("h3Product").textContent = cam.name;
   document.getElementById("descriptProduct").textContent = cam.description;
   document.getElementById("priceProduct").textContent = `${centEuro(
     cam.price
   )} €`;
-  document.getElementById("opt1").textContent = cam.lenses[0];
-  document.getElementById("opt2").textContent = cam.lenses[1];
+
+  document.getElementById("opt1").textContent = options[0];
+  document.getElementById("opt2").textContent = options[1];
+  document.getElementById("opt3").textContent = options[2];
+  document.getElementById("opt4").textContent = options[3];
 };
 displayCamera();
+
+const envoyerDonnées = async () => {
+  await fetchCamera();
+  // les données à envoyer à la page panier
+
+  // Récupérer la value de l'option choisie par user
+  const optionSelect = document.getElementById("optionsProduct");
+  const optionChoice = optionSelect.value;
+
+  //pointer le bouton et écouter au click
+  const btnAjout = document.getElementById("btnEnvoiPanier");
+  btnAjout.addEventListener("click", (event) => {
+    event.preventDefault();
+    const optionChoice = optionSelect.value;
+    let addToCard = {
+      nomProduit: cam.name,
+      idProduit: cam._id,
+      optionProduit: optionChoice,
+      quantité: 1,
+      prix: `${centEuro(cam.price)} €`,
+    };
+
+    console.log(addToCard);
+  });
+};
+envoyerDonnées();
