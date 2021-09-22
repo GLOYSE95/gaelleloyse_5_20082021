e5dc44c157d45c3033b4a69b5cf1f8f5f7e1070c
Gaelle Loyse <gaelleloyse@yahoo.fr>
2021-09-22 09:56:21 +0200

    Formulaire Valide

--- Front_end/js/panier.js
+++ Front_end/js/panier.js
@@ -57,15 +57,13 @@ if (localStorageOrinoco === null || localStorageOrinoco == 0) {
 
 //----selection des boutons supprimer
 let selectBtnSupprimer = document.querySelectorAll(".inputSuppression");
-console.log(selectBtnSupprimer);
 
 for (let j = 0; j < selectBtnSupprimer.length; j++) {
   selectBtnSupprimer[j].addEventListener("click", (event) => {
     event.preventDefault();
 
-    elementSupprime = localStorageOrinoco.splice([j], 1);
-    console.log(elementSupprime);
-    localStorage.setItem("produitUser");
+    localStorageOrinoco.splice([j], 1);
+    localStorage.setItem("produitUser", JSON.stringify(localStorageOrinoco));
     window.location.href = "panier.html";
   });
 }
@@ -126,37 +124,40 @@ var prixTotalHtml = `
 <div class="priceCamera panierParagraphe">Prix total de votre panier : ${totalPrix} €</div>`;
 prixTotal.innerHTML = prixTotalHtml;
 
-//----------Affichage du formulaire de commande
 //Pointer sur bouton commander et constante Html formulaire
 const btnCommander = document.getElementById("commander");
-const commanderHtml = `
 
+//-------------------------Formulaire de commande-----------------------------------------
+
+//------Ecouter au click pour Afficher et valider le formulaire
+btnCommander.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  //----------Affichage du formulaire de commande
+  //---constante Html formulaire
+  const commanderHtml = `
         <div class="h2--background">
           <h2>Formulaire de commande</h2>
         </div>
-
         <form action="" method="post" id="formCommande">
           <div class="bg-light bg-light-form">
 
             <div class="form-group">
-              
                 <label for="firstname">Prénom</label></br>
                 <input type="text" name="prenom" class="col-6 inputLetter inputFormulaire miInput" id="firstName">
                 <p id="errorFirstName" class="error"></p>
+                
                 <label for="lastname">Nom</label> </br>
                 <input required type="text" name="nom" class="col-6 inputLetter inputFormulaire miInput" id="lastName">
                 <p id="errorLastName" class="error"></p>
-                
               
                 <label for="email">E-mail</label></br>
-                <input required type="email" name="email" class="col-6 inputFormulaire miInput" id="email">
-                <p id="errorEmail" class="error"></p>
-                
+                <input required type="email" name="email" class="col-6 inputFormulaire miInput" id="email" placeholder="nomprenom@gmail.com">
+                <p id="errorMail" class="error"></p>
               
                 <label for="address">Adresse de livraison</label></br>
-                <input required type="text" name="adresseLiv" class="col-12 inputFormulaire adressInput" id="address">
+                <input required type="text" name="adresseLiv" class="col-12 inputFormulaire adressInput" id="address" placeholder="10 avenue de la mare">
                 <p id="errorAddress" class="error"></p>
-                
               
                 <label for="city">Ville</label></br>
                 <input required type="text" name="villeLiv" class="col-6 inputLetter inputFormulaire miInput" id="city">
@@ -168,83 +169,153 @@ const commanderHtml = `
             <label for="cgv">J'accepte les conditions générales de vente</label>
             <p id="errorCGV" class="error"></p>
             <input type="submit" name="validate" value="Valider votre commande" class="inputFormulaire inputValidation" id="validerCommande">
-          </form>
-
-            
+          </form>  
       </article>`;
-//------Ecouter au click pour Afficher le formulaire
-btnCommander.addEventListener("click", (event) => {
-  event.preventDefault();
 
-  //------Pointer sur la div container
+  //------Pointer sur la div container et afficher HTML
   const formulaireAffichage = document.getElementById("formulaireContact");
   formulaireAffichage.innerHTML = commanderHtml;
 
-  //-------------------------Formulaire de commande
-  //Ecouter au click : pour enregistrer les valeurs du formulaire dans le local storage
-
-  //selectionner le btn valider
+  //------Pointer le btn valider
   const validerLaCommande = document.getElementById("validerCommande");
-
-  //Ecouter au click
-  validerLaCommande.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    //Creation objet contact avec donnés user
-    let contact = {};
-    class contactForm {
-      constructor() {
-        this.firstName = document.getElementById("firstName").value;
-        this.lastName = document.getElementById("lastName").value;
-        this.email = document.getElementById("email").value;
-        this.address = document.getElementById("address").value;
-        this.city = document.getElementById("city").value;
-      }
-    }
-    contact = new contactForm();
-    console.log(contact);
-  });
 
   //---------------------validation du formulaire
   /// utilisation des Expressions régulières Regex
 
-  //Pointer le formulaire et l'input
-  const leFormulaire = document.getElementById("formCommande");
-  //Pointer l'input firstname LastName city
-  const inputLetter = document.getElementsByClassName("inputLetter");
-  //console.log(inputLetter);
   const leFormulairePrenom = document.getElementById("firstName");
   const leFormulaireNom = document.getElementById("lastName");
   const leFormulaireCity = document.getElementById("city");
+  const leFormulaireMail = document.getElementById("email");
+  const leFormulaireAddress = document.getElementById("address");
+  const leFormulaireCGV = document.getElementById("cgv");
+  let errorMail = document.getElementById("errorMail");
+  let errorFirstName = document.getElementById("errorFirstName");
+  let errorLastName = document.getElementById("errorLastName");
+  let errorCity = document.getElementById("errorCity");
+  let errorAddress = document.getElementById("errorAddress");
 
-  //Ecouter au click change sur l'input
-  leFormulairePrenom.addEventListener("change", function () {
-    let firstNameRegex = /^[A-Z-a-zëïö]+$/;
+  let firstNameRegex = /^[A-Z-a-zèéëïö]+$/;
+  let mailRegex = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/;
+  let addressRegex = /^[0-9]{1,3}(?:(?:[,. ]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)+$/;
 
-    const errorRequired = () => {
-      let errorFirstName = document.getElementById("errorFirstName");
+  //1 - Ecouter au click change sur l'input Prénom
+  function prenomValide() {
+    if (this.value == "") {
       errorFirstName.innerHTML = "Champs requis";
       errorFirstName.style.color = "red";
-      return false;
-    };
-    const inputValide = () => {
-      console.log("true");
+    } else if (firstNameRegex.test(this.value)) {
       errorFirstName.innerHTML = "Champs valide, merci";
       errorFirstName.style.color = "green";
-      return true;
-    };
-    const errorJustLetter = () => {
-      errorFirstName.innerHTML = "Seuls les lettres sont autorisés";
-      errorFirstName.style.color = "red";
-      return false;
-    };
-
-    if (leFormulairePrenom.value == "") {
-      errorRequired();
-    } else if (firstNameRegex.test(leFormulairePrenom.value)) {
-      inputValide();
     } else {
-      errorJustLetter();
+      errorFirstName.innerHTML =
+        "Seules les lettres sans espaces sont autorisés";
+      errorFirstName.style.color = "red";
+    }
+  }
+  leFormulairePrenom.addEventListener("change", prenomValide);
+
+  //2 - Ecouter au click change sur l'input Nom
+  function nomValide() {
+    if (this.value == "") {
+      errorLastName.innerHTML = "Champs requis";
+      errorLastName.style.color = "red";
+    } else if (firstNameRegex.test(this.value)) {
+      errorLastName.innerHTML = "Champs valide, merci";
+      errorLastName.style.color = "green";
+    } else {
+      errorLastName.innerHTML =
+        "Seules les lettres sans espaces sont autorisés";
+      errorLastName.style.color = "red";
+    }
+  }
+  leFormulaireNom.addEventListener("change", nomValide);
+
+  //3 - Ecouter au click change sur l'input City
+  function cityValide() {
+    if (this.value == "") {
+      errorCity.innerHTML = "Champs requis";
+      errorCity.style.color = "red";
+    } else if (firstNameRegex.test(this.value)) {
+      errorCity.innerHTML = "Champs valide, merci";
+      errorCity.style.color = "green";
+      return true;
+    } else {
+      errorCity.innerHTML = "Seules les lettres sans espaces sont autorisés";
+      errorCity.style.color = "red";
+    }
+  }
+  leFormulaireCity.addEventListener("change", cityValide);
+
+  //4 - Ecouter au click change sur l'input Mail
+  function mailValide() {
+    if (this.value == "") {
+      errorMail.innerHTML = "Champs requis";
+      errorMail.style.color = "red";
+    } else if (mailRegex.test(this.value)) {
+      errorMail.innerHTML = "Champs valide, merci";
+      errorMail.style.color = "green";
+      return true;
+    } else {
+      errorMail.innerHTML = "Veuillez saisir une adresse mail valide";
+      errorMail.style.color = "red";
+    }
+  }
+  leFormulaireMail.addEventListener("change", mailValide);
+
+  //5 - Ecouter au click change sur l'input Adress
+  function addressValide() {
+    if (this.value == "") {
+      errorAddress.innerHTML = "Champs requis";
+      errorAddress.style.color = "red";
+    } else if (addressRegex.test(this.value)) {
+      errorAddress.innerHTML = "Champs valide, merci";
+      errorAddress.style.color = "green";
+      return true;
+    } else {
+      errorAddress.innerHTML = "Veuillez saisir une adresse postale valide";
+      errorAddress.style.color = "red";
+    }
+  }
+  leFormulaireAddress.addEventListener("change", addressValide);
+
+  //6 - vérifier validation CGV
+  function check() {
+    if (leFormulaireCGV.checked) {
+      leFormulaireCGV = true;
+    } else {
+      return false;
+    }
+  }
+  //Ecouter au click : pour enregistrer les valeurs du formulaire dans l'objet
+  validerLaCommande.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    if (
+      firstNameRegex.test(leFormulairePrenom.value) &&
+      firstNameRegex.test(leFormulaireNom.value) &&
+      firstNameRegex.test(leFormulaireCity.value) &&
+      mailRegex.test(leFormulaireMail.value) &&
+      addressRegex.test(leFormulaireAddress.value) &&
+      leFormulaireCGV.checked
+    ) {
+      //Creation objet contact avec donnés user
+      let contact = {};
+      class contactForm {
+        constructor() {
+          this.firstName = document.getElementById("firstName").value;
+          this.lastName = document.getElementById("lastName").value;
+          this.email = document.getElementById("email").value;
+          this.address = document.getElementById("address").value;
+          this.city = document.getElementById("city").value;
+        }
+      }
+      contact = new contactForm();
+      console.log(contact);
+      let dataAEnvoyer = { contact, localStorageOrinoco };
+    } else {
+      alert(
+        "Veuillez bien remplir le formulaire et accepter les conditions générales de vente"
+      );
     }
   });
 });
